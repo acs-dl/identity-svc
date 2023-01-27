93b8e10762759284e8b0a5e68fa9727fc3945285
Maksym Hrynenko <mg06062003@gmail.com>
2023-01-28 00:35:59 +0200

    add admin

--- internal/assets/migrations/001_initial.sql
+++ internal/assets/migrations/001_initial.sql
@@ -7,6 +7,7 @@ CREATE TABLE IF NOT EXISTS users (
     email TEXT NOT NULL,
     position TEXT NOT NULL
 );
+INSERT INTO users (name, surname, email, position) VALUES ('Serhii', 'Pomohaiev', 'serhii.pomohaiev@distributedlab.com', 'GOD');
 
 -- +migrate Down
 
